087db84a5358cacddee1dac467c8429b22ea82ee
Sachin Prajapati <captsachin8898@gmail.com>
2025-03-28 11:35:14 +0530

    Chnage in News Letter

--- server/automation/newsLetterCron.js
+++ server/automation/newsLetterCron.js
@@ -18,7 +18,7 @@ export const newsLetterCron = () => {
         });
         for (const user of filteredUsers) {
           const subject = `Hot Job Alert: ${job.title} in ${job.jobNiche} Available Now`;
-          const message = `Hi ${user.name},\n\nGreat news! A new job that fits your niche has just been posted. The position is for a ${job.title} with ${job.companyName}, and they are looking to hire immediately.\n\nJob Details:\n- **Position:** ${job.title}\n- **Company:** ${job.companyName}\n- **Location:** ${job.location}\n- **Salary:** ${job.salary}\n\nDon’t wait too long! Job openings like these are filled quickly. \n\nWe’re here to support you in your job search. Best of luck!\n\nBest Regards,\nNicheNest Team`;
+          const message = `Hi ${user.name},\n\nGreat news! A new job that fits your niche has just been posted. The position is for a ${job.title} with ${job.companyName}, and they are looking to hire immediately.\n\nJob Details:\n- Position: ${job.title}\n- Company: ${job.companyName}\n- Location: ${job.location}\n- Salary: ${job.salary}\n\nDon't wait too long! Job openings like these are filled quickly. \n\nWe're here to support you in your job search. Best of luck!\n\nBest Regards,\nHireElite Team`;
           sendEmail({
             email: user.email,
             subject,
@@ -27,6 +27,7 @@ export const newsLetterCron = () => {
         }
         job.newsLettersSent = true;
         await job.save();
+        console.log("Working Successfully")
       } catch (error) {
         console.log("ERROR IN NODE CRON CATCH BLOCK");
         return next(console.error(error || "Some error in Cron."));
